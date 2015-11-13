a38e2ad2ce972d423ecf66a904ba9c336a3926a1
snowiesuet <snowiesuet@gmail.com>
2015-11-13 23:02:32 +0800

    some tweak fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 function onChangeTest() {
+    let searchTemplate = ''
     let with_people = document.getElementsByName('with_people')[0].checked
     let searchText =  document.getElementsByName('searchBox')[0].value
     console.log(with_people)
@@ -7,15 +8,12 @@ function onChangeTest() {
       url: "https://api.viki.io/v4/search.json?c="+ searchText +"&per_page=5&with_people=" +with_people+ "&app=100266a&t=1440586215",
     }).done(data => {
       console.log(data)
-      console.log(name.e)
       let episodes = ''
       data.forEach(name => {
-        if (name.e) {
-          let episodes = 'Episodes:' + name.e
-        }
-        console.log(name.e, episodes)
-        let searchTemplate = '<div class="searchTemplate">'
-        + name.tt + '<div id="searchDetails"><img src="'+name.i+'"/>Type: ' +name.t +'' + episodes  +'</div>  </div>'
+        episodes = (name.e ? ('Episodes: ' + name.e) : '')
+        console.log(episodes)
+        searchTemplate = '<div class="searchTemplate">'
+        + name.tt + '<div id="searchDetails"><img src="'+name.i+'"/>Type: ' +name.t +'<br>' + episodes  +'</div>  </div>'
         $('#searchResults').append(searchTemplate)
       })
     })
