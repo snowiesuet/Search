72f3b8759709a2529d78dc3d7333761b0bddc5c3
snowiesuet <snowiesuet@gmail.com>
2015-11-14 20:53:34 +0800

    testing blur function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict'
 
+function onBlur () {
+  console.log('hello ㅠㅠ')
+  $('#searchResults').hide()
+}
+
+const searchBox = document.getElementsByName('searchBox')[0]
+searchBox.onblur = onBlur
+
 function onChange () {
     //clear the search results from previous search
     $('#searchResults').html('')
@@ -11,23 +19,28 @@ function onChange () {
     }).done(dataset => {
       if (dataset.length > 0) {
         dataset.forEach(data => {
+          //only display data when available
           const available = data.blocked ? 'No' : 'Yes'
+          const availableText = data.t === 'person' ? '' : `Available in your region: ${available}`
           const country = data.oc ? ` Country of Origin: ${(data.oc).toUpperCase()}` : ''
           const episodes = data.e ? `(${data.e} episodes)` : ''
+          const img = data.t === 'person' ? 'https://0.viki.io/pr/18231pr/86dc9e0393.jpg?x=b&a=0x0&s=780x436&q=h&e=t&f=t&cb=1' : data.i
+          //the search template for every row
           const searchTemplate =
           `<div class="searchTemplate">
             ${data.tt}
             <div id="searchDetails">
-              <img src=${data.i}/>
+              <img src=${img}/>
             <div class="searchDesc">
             Type: ${data.t} ${episodes}
-            <br> Available in your region: ${available}
+            <br> ${availableText}
             <br> ${country}
           </div></div></div>`
           $('#searchResults').append(searchTemplate)
         })
       }
       else {
+        //if no data, display this!
         $('#searchResults').html(`<div class="searchTemplate">No data available.</div>`)
       }
     })
