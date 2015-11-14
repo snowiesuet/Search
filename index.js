138e0e8b5d48b2a8cb41943d7669c20ab63ea180
snowiesuet <snowiesuet@gmail.com>
2015-11-14 16:11:19 +0800

    css/ js cleanup,

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 
 function onChangeTest() {
     $('#searchResults').html('')
-    let with_people = document.getElementsByName('with_people')[0].checked
-    let searchText =  document.getElementsByName('searchBox')[0].value
+    const withPeople = document.getElementsByName('withPeople')[0].checked
+    const searchText =  document.getElementsByName('searchBox')[0].value
+    const timeStamp = Date.now()
     $.ajax({
-      url: "https://api.viki.io/v4/search.json?c="+ searchText +"&per_page=5&with_people=" +with_people+ "&app=100266a&t=1440586215",
-    }).done(data => {
-      let episodes = ''
-      data.forEach(name => {
-        episodes = (name.e ? ('Episodes: ' + name.e) : '')
-        let searchTemplate = `<div class="searchTemplate">
-         ${name.tt} <div id="searchDetails"><img src=${name.i}/>Type: ${name.t}
-        <br> ${episodes}  </div>  </div>`
-        $('#searchResults').append(searchTemplate)
-      })
+      url: `https://api.viki.io/v4/search.json?c=${searchText}&per_page=5&with_people=${withPeople}&app=100266a&t=${timeStamp}`,
+    }).done(dataset => {
+      if (dataset.length > 0) {
+        dataset.forEach(data => {
+          let episodes = ''
+          let available = data.blocked ? 'No' : 'Yes'
+          let country = data.oc ? data.oc : ''
+          episodes = data.e ? (`(${data.e} episodes)`) : ''
+          const searchTemplate =
+          `<div class="searchTemplate">
+            ${data.tt}
+            <div id="searchDetails">
+              <img src=${data.i}/>
+            <div class="searchDesc">
+            Type: ${data.t} ${episodes}
+            <br> Available in your region: ${available}
+            <br> Country of Origin: ${country}
+          </div></div></div>`
+          $('#searchResults').append(searchTemplate)
+        })
+      }
+      else {
+        $('#searchResults').html(`<div class="searchTemplate">No data available.</div>`)
+      }
+
     })
 }
